915da93baace09132ca9524529d92d3f8d361c2c
sagittariusYX <ssdut.xiao@gmail.com>
2017-01-23 22:55:32 +0800

    Finish avatar display, finish edit profile page.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import StringField, BooleanField, TextAreaField, TextField
+from wtforms.validators import Required, DataRequired, Length
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
-    
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators = [DataRequired()])
+    about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
